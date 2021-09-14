7e7ffda1c1ab72d6272265e823147a7553dd3f8b
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-09-14 00:12:52 -0300

    feat: table responsive

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideScreen = useBreakpointValue({
+    base: false,
+    lg: true
+  });
+
   return (
     <Box>
       <Header />
@@ -29,17 +34,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
-                <Th width="8"></Th>
+                {isWideScreen && (<Th>Data de cadastro</Th>)}
+                {isWideScreen && (<Th width="8"></Th>)}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -48,9 +53,8 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">luizgustavosetten@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  04 de Abril, 2021
-                </Td>
+                {isWideScreen && (<Td>04 de Abril, 2021</Td>)}
+                {isWideScreen && (
                 <Td>
                   <Button 
                     as="a" 
@@ -62,9 +66,10 @@ export default function UserList() {
                     Editar
                   </Button>
                 </Td>
+                )}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -73,9 +78,8 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">luizgustavosetten@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  04 de Abril, 2021
-                </Td>
+                {isWideScreen && (<Td>04 de Abril, 2021</Td>)}
+                {isWideScreen && (
                 <Td>
                   <Button 
                     as="a" 
@@ -87,9 +91,10 @@ export default function UserList() {
                     Editar
                   </Button>
                 </Td>
+                )}
               </Tr>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -98,9 +103,8 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300">luizgustavosetten@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  04 de Abril, 2021
-                </Td>
+                {isWideScreen && (<Td>04 de Abril, 2021</Td>)}
+                {isWideScreen && (
                 <Td>
                   <Button 
                     as="a" 
@@ -112,6 +116,7 @@ export default function UserList() {
                     Editar
                   </Button>
                 </Td>
+                )}
               </Tr>
             </Tbody>
           </Table>
